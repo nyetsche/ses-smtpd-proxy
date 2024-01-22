6c5e3070b742891ed2bcef5dfe33dffba867ec80
Matthew Lesko <matthew.w.lesko@nasa.gov>
2024-01-22 17:02:06 -0500

    rm prometheus support

--- main.go
+++ main.go
@@ -4,16 +4,12 @@ import (
 	"bytes"
 	"flag"
 	"log"
-	"net/http"
 	"os"
 
 	"code.crute.us/mcrute/ses-smtpd-proxy/smtpd"
 	"github.com/aws/aws-sdk-go/aws"
 	"github.com/aws/aws-sdk-go/aws/session"
 	"github.com/aws/aws-sdk-go/service/ses"
-	"github.com/prometheus/client_golang/prometheus"
-	"github.com/prometheus/client_golang/prometheus/promauto"
-	"github.com/prometheus/client_golang/prometheus/promhttp"
 )
 
 const (
@@ -22,34 +18,6 @@ const (
 )
 
 var sesClient *ses.SES
-
-var (
-	emailSent = promauto.NewCounter(prometheus.CounterOpts{
-		Namespace: "smtpd",
-		Name:      "email_send_success_total",
-		Help:      "Total number of successfuly sent emails",
-	})
-	emailError = promauto.NewCounterVec(prometheus.CounterOpts{
-		Namespace: "smtpd",
-		Name:      "email_send_fail_total",
-		Help:      "Total number emails that failed to send",
-	}, []string{"type"})
-	sesError = promauto.NewCounter(prometheus.CounterOpts{
-		Namespace: "smtpd",
-		Name:      "ses_error_total",
-		Help:      "Total number errors with SES",
-	})
-	credentialRenewalSuccess = promauto.NewCounter(prometheus.CounterOpts{
-		Namespace: "smtpd",
-		Name:      "credential_renewal_success_total",
-		Help:      "Total number successful credential renewals",
-	})
-	credentialRenewalError = promauto.NewCounter(prometheus.CounterOpts{
-		Namespace: "smtpd",
-		Name:      "credential_renewal_error_total",
-		Help:      "Total number errors during credential renewal",
-	})
-)
 
 type Envelope struct {
 	from  string
@@ -65,7 +33,6 @@ func (e *Envelope) AddRecipient(rcpt smtpd.MailAddress) error {
 
 func (e *Envelope) BeginData() error {
 	if len(e.rcpts) == 0 {
-		emailError.With(prometheus.Labels{"type": "no valid recipients"}).Inc()
 		return smtpd.SMTPError("554 5.5.1 Error: no valid recipients")
 	}
 	return nil
@@ -74,7 +41,6 @@ func (e *Envelope) BeginData() error {
 func (e *Envelope) Write(line []byte) error {
 	e.b.Write(line)
 	if e.b.Len() > SesSizeLimit { // SES limitation
-		emailError.With(prometheus.Labels{"type": "minimum message size exceed"}).Inc()
 		log.Printf("message size %d exceeds SES limit of %d", e.b.Len(), SesSizeLimit)
 		return smtpd.SMTPError("554 5.5.1 Error: maximum message size exceeded")
 	}
@@ -87,7 +53,6 @@ func (e *Envelope) logMessageSend() {
 		dr[i] = *e.rcpts[i]
 	}
 	log.Printf("sending message from %+v to %+v", e.from, dr)
-	emailSent.Inc()
 }
 
 func (e *Envelope) Close() error {
@@ -99,8 +64,6 @@ func (e *Envelope) Close() error {
 	_, err := sesClient.SendRawEmail(r)
 	if err != nil {
 		log.Printf("ERROR: ses: %v", err)
-		emailError.With(prometheus.Labels{"type": "ses error"}).Inc()
-		sesError.Inc()
 		return smtpd.SMTPError("451 4.5.1 Temporary server error. Please try again later")
 	}
 	e.logMessageSend()
@@ -123,11 +86,6 @@ func makeSesClient() (*ses.SES, error) {
 func main() {
 	var err error
 
-	disablePrometheus := flag.Bool("disable-prometheus", false, "Disables prometheus metrics server")
-	prometheusBind := flag.String("prometheus-bind", ":2501", "Address/port on which to bind Prometheus server")
-
-	flag.Parse()
-
 	sesClient, err = makeSesClient()
 	if err != nil {
 		log.Fatalf("Error creating AWS session: %s", err)
@@ -138,13 +96,6 @@ func main() {
 		addr = flag.Arg(0)
 	} else if flag.NArg() > 1 {
 		log.Fatalf("usage: %s [listen_host:port]", os.Args[0])
-	}
-
-	if !*disablePrometheus {
-		sm := http.NewServeMux()
-		ps := &http.Server{Addr: *prometheusBind, Handler: sm}
-		sm.Handle("/metrics", promhttp.Handler())
-		go ps.ListenAndServe()
 	}
 
 	s := &smtpd.Server{
